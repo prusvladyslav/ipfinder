0e68ef836c7d802ec8ad55fb862fe7f05bce5a67
prusvladyslav <79951438+prusvladyslav@users.noreply.github.com>
2021-09-10 23:23:31 +0300

    master

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 let mainBtn = document.querySelector('.main-btn');
 let dataContainer = document.querySelector('.data-container');
 let getIP = 'https://api.ipify.org/?format=json'
-let getData = 'https://ipapi.co/'
-mainBtn.addEventListener('click',function() {
- fetch(getIP)
-.then(response => response.json())
-.then(ip => {
-    
-    fetch(`${getData}${ip.ip}/json/`)
-.then(response => response.json())
-    .then(data => {
-        document.querySelector('.ip-line').textContent = `Your IP: ${data.ip}`
-        document.querySelector('.country-line').textContent = ` Your Country: ${data.country_name   }`
-        document.querySelector('.region-line').textContent = ` Your Region: ${data.region}`
-        document.querySelector('.city-line').textContent = ` Your City: ${data.city}`
-        document.querySelector('.city-line').textContent = ` Your City: ${data.city}`
-        document.querySelector('.timezone-line').textContent = ` Your Timezone: ${data.timezone}`
-        console.log(data);
-       }
-   
-
-    )
-    dataContainer.style.display = 'block';
+let getData = 'http://ip-api.com/json/'
+class Card {
+    constructor(query,country,region,city,timezone,mobile) {
+        this.country = country
+        this.region = region
+        this.query = query
+        this.city = city
+        this.timezone = timezone
+        this.mobile = mobile
+    }
+render() {
+let mobileLine = document.createElement('h3');
+   if (this.mobile){
+    mobileLine.innerHTML = `<h3>You are using phone</h3>`
+   }
+   else {
+      mobileLine.innerHTML = `<h3>You are using PC</h3>`
+   }
+   const dataContainer = document.createElement('div');
+   dataContainer.classList.add('data-container') 
+   dataContainer.insertAdjacentHTML('afterbegin',
+   `<h3 class="ip-line">
+   Your IP: ${this.query}
+   </h3>
+   <h3 class="country-line">
+   Your Country: ${this.country}
+   </h3>
+   <h3 class="region-line">
+   Your Region: ${this.region}
+   </h3>
+   <h3 class="city-line">
+   Your City: ${this.city}
+   </h3>
+   <h3 class="timezone-line">
+   Your Timezone: ${this.timezone}
+   </h3>
+   ${mobileLine.innerHTML}
+   <h3 class="answer-line">
+   Обычный скрипт в JS срабатывает строчка за строчкой, когда мы начинаем работать с асинхронностью то эта концепция меняеться и формируется определленая "очередь" задач
+   </h3>`)
+document.body.append(dataContainer);
+mainBtn.remove()
+}
+}
+mainBtn.addEventListener('click', async function() {
+let response = await fetch(getIP)
+let ip = await response.json()
+let dataObj = await fetch(`${getData}${ip.ip}?fields=country,regionName,city,timezone,mobile,query`)
+let data = await dataObj.json()
+ data = new Card(data.query,data.country,data.regionName,data.city,data.timezone,data.mobile)
+ data.render()
 })
-})  
+
+  
